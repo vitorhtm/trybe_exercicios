ee8c4b6833b9c85ae1238013b08df7d16a28df9a
Vitormonteiro28 <vitormonteirotomaz7@gmail.com>
2022-06-13 11:35:36 -0300

    exercicios ok

---
+++ Front_end/bloco10_introdução_react/exercicioCriarComponente/novo_projeto/src/About.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <>
+                <h1>
+                    <p>Vitor Hugo Tomaz Monteiro</p>
+                </h1>
+            </>
+        )
+    }
+}
+
+export default HelloWorld
---
+++ Front_end/bloco10_introdução_react/exercicioCriarComponente/novo_projeto/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import HelloWorld from './About';
+
+function App() {
+  return (
+    <HelloWorld />
+  );
+}
+
+export default App;
